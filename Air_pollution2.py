1bb50f182bc83692365ee4fdaa3155c0831277fb
hojimint <83691399+hojimint@users.noreply.github.com>
2022-11-29 15:57:10 +0900

    Create Air_pollution2.py

---
+++ Air_pollution2.py
@@ -0,0 +1,42 @@
+#라이브러리 import
+import requests
+import pprint
+import json
+import pandas as pd
+
+file_path = "C:\\Users\hojin\Desktop\gwajea\python\gimal\simple.txt"
+url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=송파구&dataTerm=DAILY&pageNo=1&numOfRows=100&returnType=json&serviceKey=KQRR%2BJLPRITcRv6CvRB1QUxmDQ%2BKmcKWMjK1A19g%2BiHLEbXTpqjWmut5pwHfKkH6O7KfqLSXxEmrLt6Ctooliw%3D%3D"
+
+response = requests.get(url)
+
+contents = response.text
+
+# 데이터 결과값 예쁘게 출력해주는 코드
+pp = pprint.PrettyPrinter(indent=4)
+# print(pp.pprint(contents))
+
+## json을 DataFrame으로 변환하기 ##
+
+#문자열을 json으로 변경
+json_ob = json.loads(contents)
+# print(type(json_ob)) #json타입 확인
+
+# 필요한 내용만 꺼내기
+body = json_ob['response']['body']['items']
+# print(body)
+
+# # Dataframe으로 만들기
+dataframe = pd.json_normalize(body)
+
+# dataframe.index = ['khaiValue', 'pm10Value', 'no2Value', '03Value']
+# print(dataframe['khaiValue'])
+total = dataframe['khaiValue']
+dust = dataframe['pm10Value']
+p1 = pd.concat([total,dust],axis=1)
+# print(p1)
+st.write(p1)
+# dataframe.to_csv('9.41.csv',encoding='euc-kr')
+# print(dataframe)
+
+# with open(file_path, 'w') as f:
+#     json.dump(dataframe, f)
