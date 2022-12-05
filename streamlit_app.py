a339026a91925ba23e2985667e26a7962df1f472
hojimint <83691399+hojimint@users.noreply.github.com>
2022-12-05 18:21:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import altair as alt
 
 file_path = "C:\\Users\hojin\Desktop\gwajea\python\gimal\simple.txt"
-url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=파주&dataTerm=DAILY&pageNo=2&numOfRows=100&returnType=json&serviceKey=KQRR%2BJLPRITcRv6CvRB1QUxmDQ%2BKmcKWMjK1A19g%2BiHLEbXTpqjWmut5pwHfKkH6O7KfqLSXxEmrLt6Ctooliw%3D%3D"
+url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=파주&dataTerm=DAILY&pageNo=1&numOfRows=100&returnType=json&serviceKey=KQRR%2BJLPRITcRv6CvRB1QUxmDQ%2BKmcKWMjK1A19g%2BiHLEbXTpqjWmut5pwHfKkH6O7KfqLSXxEmrLt6Ctooliw%3D%3D"
 image = "https://github.com/hojimint/lit/blob/main/RESULT2.PNG?raw=true"
 image2 ="https://github.com/hojimint/lit/blob/main/RESULT1.PNG?raw=true"
 
@@ -34,9 +34,10 @@ dataframe = pd.DataFrame(body)
 # # key 값 int으로 만들기
 dataframe['total'] = pd.to_numeric(dataframe['khaiValue'])
 dataframe['dust'] = pd.to_numeric(dataframe['pm10Value'])
-time = dataframe['dataTime'][1]
-total = dataframe['total'][1]
-dust = dataframe['dust'][1]
+dataframe['O3'] = pd.to_numeric(dataframe['o3Value'])
+o3 = dataframe['o3Value']
+total = dataframe['total']
+dust = dataframe['dust']
 # # 바차트 올리기
 st.title('공공데이터 분석하기')
 st.write('한국환경공단_에어코리아_대기오염정보의 통합환경수치와 미세먼지를 불러와 분석을 진행합니다.')
@@ -45,5 +46,8 @@ st.header("통합 환경 수치")
 st.bar_chart(total)
 st.image(image2)
 st.header("미세먼지")
+st.bar_chart(dust)
+st.image(image)
+st.header("o3")
 st.bar_chart(dust)
 st.image(image)
