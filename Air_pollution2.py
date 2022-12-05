74b0a2ea08fc65de66854eb65d7b2ad452cc977d
hojimint <83691399+hojimint@users.noreply.github.com>
2022-12-05 09:47:36 +0900

    Update Air_pollution2.py

--- Air_pollution2.py
+++ Air_pollution2.py
@@ -28,13 +28,19 @@ body = json_ob['response']['body']['items']
 
 # # Dataframe으로 만들기
 dataframe = pd.DataFrame(body)
-# # key 값 int으로 만들기
 dataframe['total'] = pd.to_numeric(dataframe['khaiValue'])
 dataframe['dust'] = pd.to_numeric(dataframe['pm10Value'])
 time = dataframe['dataTime']
 total = dataframe['total']
 dust = dataframe['dust']
-# # 바차트 올리기
+# p1 = pd.concat([time,total,dust],axis=1)
+# st.bar_chart(p1[ ['khaiValue', 'pm10Value'] ] )
 st.write(total)
 st.bar_chart(total)
 st.bar_chart(dust)
+
+# st.line_chart(p1,width=0, height=0)
+
+# print(p1)
+# with open(file_path, 'w') as f:
+#     json.dump(dataframe, f)
